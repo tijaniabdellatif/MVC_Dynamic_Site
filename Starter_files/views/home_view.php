6d7c77d0bd6cfd1a08aa5847ba0911401187b337
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-06-27 15:43:01 +0100

    starter_files

---
+++ Starter_files/views/home_view.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+
+    <?php include_once 'views/includes/head.php'?>
+
+    <title><?= ucfirst($page) ?></title>
+</head>
+
+<body>
+
+    <?php include_once 'views/includes/header.php'?>
+
+    <!-- CONTENU -->
+
+    <?php include_once 'views/includes/footer.php'?>
+
+</body>
+</html>
